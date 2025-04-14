f8f249cb40eb6e45b2c277840e938c87b154b1e0
AntonioGoncalvesSousa <antonio.sousa@sptech.school>
2025-04-14 00:00:58 -0300

    Fazendo a função para pegar o tempo

---
+++ main.js
@@ -0,0 +1,18 @@
+const blocoSeTempo = document.querySelectorAll('.time');
+
+function plotarTempo(){
+    const data = new Date();
+
+    const hora = data.getHours().toString().padStart(2, '0');
+    const minuto = data.getMinutes().toString().padStart(2, '0');
+    const segundo = data.getSeconds().toString().padStart(2, '0');
+
+    blocoSeTempo[0].innerHTML = hora;
+    blocoSeTempo[1].innerHTML = minuto;
+    blocoSeTempo[2].innerHTML = segundo;
+
+}
+
+setInterval(plotarTempo, 1000);
+
+plotarTempo();
