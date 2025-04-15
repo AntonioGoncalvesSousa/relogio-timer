6e909ee84e043d7e6c513b807257f98fdfdc9e36
AntonioGoncalvesSousa <antonio.sousa@sptech.school>
2025-04-15 20:24:40 -0300

    Mudando hora para ms e implementando logica de play e pause

--- timer/timer.js
+++ timer/timer.js
@@ -1,6 +1,33 @@
+const segundoDisplay = document.querySelector('#segundo')
+const minutoDisplay = document.querySelector('#minuto')
+const milisegundoDisplay = document.querySelector('#milisegundo')
 
-function startTimer(button){
+let startTime = 0;
+let tempoAnterior = 0;
+let intervalo;
+
+function startTimer(button) {
     button.classList.toggle('stop');
 
-    console.log(button.classList);
+    if (button.classList.contains('stop')) {
+        // TIMER RODANDO
+        startTime = performance.now() - tempoAnterior;
+
+        intervalo = setInterval(() => {
+            const tempoAtual = performance.now();
+            const tempoDecorrido = tempoAtual - startTime;
+
+            const minuto = Math.floor(tempoDecorrido / 60000);
+            const segundo = Math.floor((tempoDecorrido % 60000) / 1000);
+            const milisegundo = Math.floor(tempoDecorrido % 1000);
+
+            minutoDisplay.innerHTML = minuto.toString().padStart(2, '0');
+            segundoDisplay.innerHTML = segundo.toString().padStart(2, '0');
+            milisegundoDisplay.innerHTML = milisegundo.toString().padStart(3, '0');
+        }, 10);
+    } else {
+        // TIMER PARADO
+        clearInterval(intervalo);
+        tempoAnterior = performance.now() - startTime;
+    }
 }
